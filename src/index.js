9ed70cf32d48da211ff8ce4b84fdd971964430c4
LucianoGiraldi <luciano.texugo@gmail.com>
2025-06-15 22:53:59 -0300

    chore(index): configurar servidor Express e uso de rotas protegidas

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
 const taskRoutes = require('./routes/task.routes');
+const userRoutes = require('./routes/user.routes');
 const authMiddleware = require('./middlewares/auth.middleware');
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 connectDB();
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', authMiddleware, userRoutes);
 app.use('/api/tasks', authMiddleware, taskRoutes);
 
 const PORT = process.env.PORT || 3000;
